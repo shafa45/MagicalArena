9d7b0181635a06901fe27a1c7ecf0d6df16acd3d
shafa45 <safaullah14@gmail.com>
2024-09-21 21:49:47 +0530

    Add Player class with health, strength, and attack attributes.

---
+++ src/main/java/MagicalArena/Player.java
@@ -0,0 +1,33 @@
+package MagicalArena;
+
+public class Player {
+    private int health;
+    private int strength;
+    private int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void reduceHealth(int amount) {
+        this.health = Math.max(0, this.health - amount);
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+}
