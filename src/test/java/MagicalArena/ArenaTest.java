41a3fbd8a85c3421968fa6360d31041e21683e30
shafa45 <safaullah14@gmail.com>
2024-09-22 16:14:15 +0530

    Add unit tests for Player class to verify health reduction and initialization.

--- src/test/java/MagicalArena/ArenaTest.java
+++ src/test/java/MagicalArena/ArenaTest.java
@@ -12,4 +12,13 @@ public class ArenaTest {
         assertEquals(5, player.getStrength());
         assertEquals(10, player.getAttack());
     }
+
+    @Test
+    public void testPlayerHealthReduction() {
+        Player player = new Player(50, 5, 10);
+        player.reduceHealth(20);
+        assertEquals(30, player.getHealth());
+        player.reduceHealth(40);
+        assertEquals(0, player.getHealth());
+    }
 }
